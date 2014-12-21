53c657e14276c6df183b991e89aa435c035dbd51
denik7 <softdenik@hotmail.com>
2014-12-21 19:03:52 +0100

    Create MainClass.java

---
+++ GUI/MainClass.java
@@ -0,0 +1,16 @@
+/*
+ * Clase principal del programa (ejecutable).
+ */
+package GUI;
+
+import javax.swing.JFrame;
+
+public class MainClass {
+
+    public static void main(String[] args) {
+        
+        PanelMultiple pm = new PanelMultiple();
+        pm.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        
+    }
+}
